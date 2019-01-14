d6b1cdf74ac682cb3065e746f5653a57915e866c
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-01-14 18:56:14 -0500

    se documento un poco el template dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+/*in order to use this router with others components or applications
+such as login, signup etc, I've created a main component called DashboardComponent
+so that is de mainly entry point application.
+*/
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
